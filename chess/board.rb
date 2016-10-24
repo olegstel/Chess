fe77102a226b8e94e8c9a647d2235424cebe1dc4
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2016-10-24 14:36:51 -0400

    Write piece and board classes

---
+++ chess/board.rb
@@ -0,0 +1,16 @@
+class Board
+
+def initialize()
+  @grid = create_grid
+  populate_board
+end
+
+def create_grid
+  Array.new(8) { Array.new(8)}
+end
+
+def populate_board
+  
+end
+
+end
---
+++ chess/piece.rb
@@ -0,0 +1,25 @@
+class Piece
+
+end
+
+
+class NullPiece < Piece
+end
+
+class Pawn < Piece
+end
+
+class Kinght < Piece
+end
+
+class Bishop < Piece
+end
+
+class Queen < Piece
+end
+
+class King < Piece
+end
+
+class Rook < Piece
+end
