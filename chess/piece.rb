a8fcef35a805a7bf8056068d351c3f050c6e0627
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2016-10-24 15:13:20 -0400

    Pass piece a color variable

--- chess/piece.rb
+++ chess/piece.rb
@@ -1,5 +1,9 @@
 class Piece
 
+  def initialize(position, color)
+    @position = position
+    @color = color
+  end
 end
 
 
@@ -7,19 +11,37 @@ class NullPiece < Piece
 end
 
 class Pawn < Piece
+  def to_s
+    "P"
+  end
 end
 
-class Kinght < Piece
+class Knight < Piece
+  def to_s
+    "H"
+  end
 end
 
 class Bishop < Piece
+  def to_s
+    "B"
+  end
 end
 
 class Queen < Piece
+  def to_s
+    "Q"
+  end
 end
 
 class King < Piece
+  def to_s
+    "K"
+  end
 end
 
 class Rook < Piece
+  def to_s
+    "R"
+  end
 end
